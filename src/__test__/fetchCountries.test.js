741733217c3519759acae708e2322d58ba83c2ce
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-01-08 02:18:06 +0100

    Add more tests for countries data structure

--- src/__test__/fetchCountries.test.js
+++ src/__test__/fetchCountries.test.js
@@ -9,10 +9,18 @@ describe('Country data is well structured', () => {
     });
   });
   it('is made of regular countries', async () => {
-    const regCtrs = ['Afghanistan', 'Kenya', 'Latvia'];
+    const regCtrs = ['Afghanistan', 'Kenya', 'Latvia', 'Vietnam'];
     await fetchCountries().then((countries) => {
       expect(() => {
         regCtrs.every((rc) => countries.find((ctr) => ctr.name === rc));
+      }).toBeTruthy();
+    });
+  });
+  it('includes countries blocs', async () => {
+    const ctrBlocs = ['Arab World', 'Europe & Central Asia', 'Pre-demographic dividend'];
+    await fetchCountries().then((countries) => {
+      expect(() => {
+        ctrBlocs.every((rc) => countries.find((ctr) => ctr.name === rc));
       }).toBeTruthy();
     });
   });
